2c8e7a8a7e20680446380d0cb7ed27e2039fa4c6
gengrui01 <1207198609@qq.com>
2021-03-05 17:02:18 +0800

    引入AOP(XML)

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import services.AccountService;
@@ -17,7 +16,6 @@ import services.AccountService;
 public class AccountTest {
 
     @Autowired
-    @Qualifier("transactionProxyAccountService")
     private AccountService accountService;
 
     @Test
